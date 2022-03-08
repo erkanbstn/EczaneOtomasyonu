b4db2270d3b9ddfd595927dacede9b935198cc06
Erkan Bostan <profesyonel59@gmail.com>
2022-03-08 04:45:28 +0300

    Update Cache.cs

--- CC-Pharma/Cache.cs
+++ CC-Pharma/Cache.cs
@@ -9,7 +9,7 @@ namespace CC_Pharma
 {
     public static class Cache
     {
-        public static SqlConnection bgl = new SqlConnection("Data Source=94.73.145.8;Initial Catalog=u0241506_EczCV; User Id=u0241506_Geo2;Password=MAmh01F2YDqc74U;MultipleActiveResultSets=True");
+        public static SqlConnection bgl = new SqlConnection("");
         public static string Ilac { get; set; }
         public static string Isim { get; set; }
         public static bool YK { get; set; }
